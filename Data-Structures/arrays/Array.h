7bb21e09599c56f62c84c8f722a389eb2d350519
RajaATAli <allmdararslan13@gmail.com>
2023-10-15 23:51:32 -0400

    Array.h added

---
+++ Data-Structures/arrays/Array.h
@@ -0,0 +1,28 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+#include <iostream>
+
+class Array {
+  // Add Member Attributes Here (characteristic/property)
+  private:
+  int* data; // Dynamically allocate memory for storing integer elements
+  int size; // Total number of elements array can hold
+  int length; // Number of elements currently in the array
+
+
+  // Add Methods here (actions/behaviors)
+  /*
+  1. Insert -> Add an element at specified position
+  2. Delete -> Remove an element from specified position
+  3. Access -> Retrieve an element at a specified position
+  4. Update -> Modify an element at a specified position
+  4. Resize -> Increase or decrese the size of the array
+
+  */
+  public:
+
+};
+
+
+
+#endif
