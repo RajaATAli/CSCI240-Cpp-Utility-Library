783f34924aff14da254bec4df8ffb51ce5b5c485
RajaATAli <allmdararslan13@gmail.com>
2023-10-16 14:25:13 -0400

    Templated class in Array.h implemented

--- Data-Structures/arrays/Array.h
+++ Data-Structures/arrays/Array.h
@@ -1,11 +1,12 @@
 #ifndef ARRAY_H
 #define ARRAY_H
-#include <iostream>
+
+template<typename T>
 
 class Array {
   // Add Member Attributes Here (characteristic/property)
   private:
-  int* data; // Dynamically allocate memory for storing integer elements
+  T* data; // Dynamically allocate memory
   int size; // Total number of elements array can hold
   int length; // Number of elements currently in the array
 
@@ -20,9 +21,87 @@ class Array {
 
   */
   public:
+  // Constructor
+  Array();
+  Array(int s);
+
+  // Destructor
+  ~Array();  
+    
+  // Prototypes
+  void Insert(int index, const T& element);
+  void DeleteAt(int index);
+  T Access(int index) const;
+  void Update(int index, const T& value);
+  void Resize(int new_size);
 
 };
 
+// Default Constructor
+template<typename T>
+Array<T>::Array() {
+    size = 100;
+    length = 0;
+    data = new T[100];
+}
 
+// Parameterized Constructor
+template<typename T>
+Array<T>::Array(int s) {
+    size = s;
+    length = 0;
+    data = new T[s];
+}
+
+// Destructor
+template<typename T>
+Array<T>::~Array() {
+    delete[] data;
+}
+
+template<typename T>
+void Array<T>::Insert(int index, const T& element) {
+    if (index < 0 || index > size) {
+        std::cerr << "Error: Cannot insert because the index is out of bounds." << std::endl;
+        return;
+    }
+      
+    if (length == size) {
+        std::cerr << "Error: Cannot insert more as the array is full." << std::endl;
+        return;
+    }
+    // Shifting elements by one after the index
+    // Start from the end to make sure you don't overwrite anything
+    for (int i = length; i > index; i--) {
+        data[i] = data[i-1];
+    }
+      
+    data[index] = element;
+    length++;
+}
+
+// Other function implementations
+
+// Check index validity
+// Shift elements to left starting from index?
+template<typename T>
+void Array<T>::DeleteAt(int index){
+
+}
+
+template<typename T>
+T Array<T>::Access(int index) const{
+
+}
+
+template<typename T>
+void Array<T>::Update(int index, const T& value){
+
+}
+
+template<typename T>
+void Array<T>::Resize(int new_size){
+
+}
 
 #endif
