84ef399961cf2049ee4250bf52ca03624baef3fb
RajaATAli <allmdararslan13@gmail.com>
2023-10-20 12:40:35 -0400

    Array.h file completed

--- Data-Structures/arrays/Array.h
+++ Data-Structures/arrays/Array.h
@@ -59,6 +59,7 @@ Array<T>::~Array() {
     delete[] data;
 }
 
+// Inserting
 template<typename T>
 void Array<T>::Insert(int index, const T& element) {
     if (index < 0 || index > size) {
@@ -80,28 +81,82 @@ void Array<T>::Insert(int index, const T& element) {
     length++;
 }
 
-// Other function implementations
-
-// Check index validity
-// Shift elements to left starting from index?
+// You are deleting the element, not replacing it
 template<typename T>
 void Array<T>::DeleteAt(int index){
+    // Check for valid index
+    if (index < 0 || index >= length){
+        std::cerr << "Error: Cannot insert because the index is out of bounds." << std::endl;
+        return;
+    }
 
+    // Decrease length by one because that specific block will be deleted
+    // Actually the last block is deleted because the array to the right of the index will take its place (shift to the left from deleted index)
+    for (int i = index; i < length - 1; i++){
+        data[i] = data[i+1];
+    }
+
+    length --;
 }
+
+// Other function implementations
 
 template<typename T>
 T Array<T>::Access(int index) const{
+    if (index < 0 || index >= length){
+        std::cerr << "Error: Cannot access because the index is out of bounds." << std::endl;
+        return T(); // Default constructed value
+    }
+    
+    else {
+        return data[index];
+    }
 
 }
 
 template<typename T>
 void Array<T>::Update(int index, const T& value){
+    if (index < 0 || index >= length){
+        std::cerr << "Error: Cannot update because the index is out of bounds." << std::endl;
+        return;
+    }
+
+    else {
+        data[index] = value;
+    }
 
 }
 
+
+// You should be altering the capacity (`size`) and not `length`
 template<typename T>
 void Array<T>::Resize(int new_size){
+    // Check if the new size is valid
+    if (new_size <= 0) {
+        std::cerr << "Error: Invalid size provided for resize." << std::endl;
+        return;
+    }
 
+    // Allocate new memory
+    T* new_data = new T[new_size];
+
+    // Copy old data to the new block
+    for (int i = 0; i < std::min(length, new_size); i++) {
+        new_data[i] = data[i];
+    }
+
+    // Delete old data
+    delete[] data;
+
+    // Update the data pointer and size
+    data = new_data;
+    size = new_size;
+
+    // If the new size is smaller than the current length, adjust the length
+    if (new_size < length) {
+        length = new_size;
+    }
 }
+
 
 #endif
