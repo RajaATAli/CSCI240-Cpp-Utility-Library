32ca18296eb7b399853f3a793c5593bc52f1a900
RajaATAli <allmdararslan13@gmail.com>
2023-11-25 15:19:55 -0500

    Dynamic Array Visualization Updated

--- Web-Visualization/Data-Structures/Array/script.js
+++ Web-Visualization/Data-Structures/Array/script.js
@@ -3,8 +3,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Functions to add and remove elements
   function addElement() {
-    const newValue = Math.floor(Math.random() * 100);
-    arrayData.push(newValue);
+    const value = parseInt(document.getElementById('newValue').value, 10);
+    const index = parseInt(document.getElementById('newIndex').value, 10);
+  
+    // Check if the index is valid
+    if (isNaN(index) || index < 0 || index > arrayData.length) {
+      alert("Invalid index. Please enter a valid index.");
+      return;
+    }
+  
+    // Check if the value is valid
+    if (isNaN(value)) {
+      alert("Please enter a valid number.");
+      return;
+    }
+  
+    // Add element at the specified index
+    arrayData.splice(index, 0, value);
     updateArray();
   }
 
@@ -18,10 +33,49 @@ document.addEventListener('DOMContentLoaded', function() {
     // Adjust SVG container size based on array length
     svg.attr('width', arrayData.length * (barWidth + barSpacing));
 
-    // Update the bars, text labels, and indexes
-    // [Include logic here for handling the enter, update, and exit selections]
-    // Don't forget to add transitions for animating changes
-  }
+    // Update the bars (elements)
+    const bars = svg.selectAll('rect').data(arrayData);
+    bars.enter()
+        .append('rect')
+        .merge(bars)
+        .transition()
+        .duration(500)
+        .attr('x', (d, i) => i * (barWidth + barSpacing))
+        .attr('y', 20)
+        .attr('width', barWidth)
+        .attr('height', 30)
+        .attr('fill', '#4CAF50');
+    bars.exit().remove();
+
+    // Update text labels (values)
+    const values = svg.selectAll('.value').data(arrayData);
+    values.enter()
+        .append('text')
+        .attr('class', 'value')
+        .merge(values)
+        .transition()
+        .duration(500)
+        .text(d => d)
+        .attr('x', (d, i) => i * (barWidth + barSpacing) + barWidth / 2)
+        .attr('y', 40)
+        .attr('text-anchor', 'middle');
+    values.exit().remove();
+
+    // Update index labels (indexes)
+    const indexes = svg.selectAll('.index').data(arrayData);
+    indexes.enter()
+        .append('text')
+        .attr('class', 'index')
+        .merge(indexes)
+        .transition()
+        .duration(500)
+        .text((d, i) => `Index ${i}`)
+        .attr('x', (d, i) => i * (barWidth + barSpacing) + barWidth / 2)
+        .attr('y', 70)
+        .attr('text-anchor', 'middle');
+    indexes.exit().remove();
+}
+
 
   // Event listeners for buttons
   document.getElementById('addElement').addEventListener('click', addElement);
